f147e2ab6de56dad3ec9ffcd86b38cbe8972a113
Martin <93378698+MrtinSt@users.noreply.github.com>
2024-03-28 11:21:20 +0100

    Update distributedSystem.sh
    add changes from lesson yesterday

--- distributedSystem.sh
+++ distributedSystem.sh
@@ -1,37 +1,44 @@
 #!/bin/bash
 
+#in crontab -e nachfolgendes einfügen: */1 * * * * bash /home/pi/DFMS/distributedSystem.sh upload >> /home/pi/DFMS/logging.txt &
+
 #programm kann mit upload oder download aufgerufen werden
 
 #erstelle slaves.txt mit in jeder zeile einer ip zum anfragen
 #erstelle config.txt mit username (erste zeile) und passwort (zweite zeile), rootdirectory (dritte zeiel) und  remote directory (vierte zeile)
 
 #startzeit speichern
+SECONDS=0
 start=$(date +%s.%N)
 echo "Startzeit: $(date +"%r")"
 
 # Dateiname für die IP-Adressen
-ip_file="slaves.txt"
-config_file="config.txt"
+ip_file="/home/pi/DFMS/slaves.txt"
+config_file="/home/pi/DFMS/config.txt"
 
 
 # Überprüfen, ob mindestens ein Parameter übergeben wurde
 if [ $# -eq 0 ]; then
-    echo "$(date +"%r") Keine Parameter übergeben. Bitte Parameter angeben."
+    echo "Keine Parameter übergeben. Bitte Parameter angeben."
     exit 1
 fi
 
 # Erster übergebener Parameter ist $1, zweiter ist $2 usw.
 command="$1"
-echo "$(date +"%r") Kommando: $command"
+echo "Kommando: $command"
 
 
 # Überprüfen, ob die Datei existiert
-if [[ ! -f $ip_file ]]; then
-  echo "$(date +"%r") Die angegebene IP-Adress Datei existiert nicht."
+if [[ -f $ip_file ]]; then
+  echo "IP-Adressdatei existiert"
+else
+  echo "$(date +"%r") Die angegebene IP-Adress Datei existiert nicht. huhu"
   exit 1
 fi
 
-if [[ ! -f $config_file ]]; then
+if [[ -f $config_file ]]; then
+  echo "Config Datei existiert"
+else
   echo "$(date +"%r") Die angegebene Konfig Datei existiert nicht."
   exit 1
 fi
@@ -52,20 +59,22 @@ remoteDirectory=$(sed -n '4p' $config_file)
 # Ausgabe der gespeicherten Informationen nur debugging
 #echo "Benutzername: $username"
 #echo "Passwort: $password"
-echo "$(date +"%r") root directory: $rootDirectory"
-echo "$(date +"%r") remote directory: $remoteDirectory"
+echo "root directory: $rootDirectory"
+echo "remote directory: $remoteDirectory"
 
 # Zeilenweise den Inhalt der Datei ausgeben
 while IFS= read -r ip; do
-  # track process
-  ./track_process.sh $$ &
-  
+
   #nur für debugging
   #echo "$ip"
+  
+  # track process
+  ./track_process.sh $$ &
 
   #ist die ip erreichbar?
   if ping -c 1 $ip &> /dev/null; then
-    echo "$ip ist erreichbar."
+    echo "$(date +"%r") $ip ist erreichbar."
+
 
     # Kommando anzeigen
     # echo "Komando: $command"
@@ -74,56 +83,52 @@ while IFS= read -r ip; do
     if [ "$command" == "upload" ]; then
       # uploaden der dateien
 
-      #HIER IST EIN FEHLER!!!
       #rootDirectory="~/Files/"
       #sendingCommand="$rootDirectory $username@$ip:/home/admin/VTDS/Files/"
       #echo "Bashcommand: $sendingCommand"
-      sshpass -p $password rsync -r $(pwd)$rootDirectory $username@$ip:$(pwd)$remoteDirectory
+      sshpass -p $password rsync -r $HOME$rootDirectory $username@$ip:$HOME$remoteDirectory
       #sshpass -p $password rsync -r ~/VTDS/Files/ $username@$ip:~/VTDS/Files/
-      echo "$(date +"%r") Raspberry Pi upload fertig"
-      #wenn raspi gefunden und synchronisiert, abbrechen
       
       # Endzeit speichern
-      end=$(date +%s.%N)
+      #end=$(date +%s.%N)
+      echo "$(date +"%r") Raspberry Pi upload fertig"
       #laufzeit berechnen
-      runtime=$(echo "$end - $start" | bc)
-      echo "Ausfueherungseit: $runtime s"
-    
+      #runtime=$(echo "$end - $start" | bc)
+      echo "Ausfueherungseit: $SECONDS s"
+      #wenn raspi gefunden und synchronisiert, abbrechen
       exit 1
     elif [ "$command" == "download" ]; then
       #downloaden der dateien
-      sshpass -p $password rsync -r $username@$ip:$(pwd)$remoteDirectory $(pwd)$rootDirectory
+      sshpass -p $password rsync -r $username@$ip:$HOME$remoteDirectory $HOME$rootDirectory
+      
+      # Endzeit speichern
+      #end=$(date +%s.%N)
       echo "$(date +"%r") Raspberry Pi download fertig"
-      
-      # Endzeit speichern
-      end=$(date +%s.%N)
-      #laufzeit berechnen
-      runtime=$(echo "$end - $start" | bc)
-      echo "Ausfueherungseit: $runtime s"
-      
+      # Berechnung der Laufzeit
+      #runtime=$(echo "$end - $start" | bc)
+      echo "Ausfuehrungszeit: $SECONDS s"
     else
-      echo "$(date +"%r") Ungültiges Kommando. Verwenden Sie 'download' oder 'upload'."
+      echo "Ungültiges Kommando. Verwenden Sie 'download' oder 'upload'."
       # Endzeit speichern
-      end=$(date +%s.%N)
-      #laufzeit berechnen
-      runtime=$(echo "$end - $start" | bc)
-      echo "Ausfueherungseit: $runtime s"
+      #end=$(date +%s.%N)
+
+      # Berechnung der Laufzeit
+      #runtime=$(echo "$end - $start" | bc)
+      echo "Ausfuehrungszeit: $SECONDS s"
       exit 1
     fi
     break
   else
     echo "$(date +"%r") $ip nicht erreichbar"
-    # Endzeit speichern
-    end=$(date +%s.%N)
-    #laufzeit berechnen
-    runtime=$(echo "$end - $start" | bc)
-    echo "Ausfueherungseit: $runtime s"
   fi
 
 done < "$ip_file"
 
+
 # Endzeit speichern
-end=$(date +%s.%N)
-#laufzeit berechnen
-runtime=$(echo "$end - $start" | bc)
-echo "Ausfueherungseit: $runtime s"
+#end=$(date +%s.%N)
+
+# Berechnung der Laufzeit
+#runtime=$(echo "$end - $start" | bc)
+
+echo "Ausfuehrungszeit $SECONDS s"
